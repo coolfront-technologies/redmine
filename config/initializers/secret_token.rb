9f3a303dfdc5fa529d5fee5892ae5c5fe9e47bed
Aaron Claypoole <aaron@coolfront.com>
2022-07-12 10:33:23 -0400

    Change secret key base env var name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -8,4 +8,4 @@
 # change this key, all old sessions will become invalid! Make sure the
 # secret is at least 30 characters and all random, no regular words or
 # you'll be exposed to dictionary attacks.
-RedmineApp::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+RedmineApp::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
