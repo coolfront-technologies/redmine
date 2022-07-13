6dbe7dbaf8d9b171279744a36e9aae38f9f09eba
Aaron Claypoole <aaron@coolfront.com>
2022-07-13 11:35:03 -0400

    Attempt to fix github_commit_logger to work with Redmine 4.2.x again 2

--- plugins/github_commit_logger/init.rb
+++ plugins/github_commit_logger/init.rb
@@ -1,11 +1,9 @@
 require 'redmine'
-require 'journal_patch'
+require 'github_commit_logger_journal_patch'
 
 
 Redmine::Plugin.register :github_commit_logger do
   name        'Github Commit Logger'
   author      'Matt Smith'
   description "When Github tries to updates an issue, create a changeset instead."
-
-  Journal.send(:include, JournalPatch)
 end
