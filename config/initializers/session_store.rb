f26c109859179eee3b862a704eb327ec8f4b1b4b
Sarita Sharma <sarita.sharma@xplortechnologies.com>
2026-01-23 16:36:51 -0500

    The session store configuration has been updated to set secure: true and same_site: :none for the session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Explicitly set the session store for Redmine
-RedmineApp::Application.config.session_store :cookie_store, key: '_redmine_session', domain: :all
+RedmineApp::Application.config.session_store :cookie_store, key: '_redmine_session', domain: :all, secure: true, same_site: :none
