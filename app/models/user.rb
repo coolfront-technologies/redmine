02115b3324ac95feae7bd5dc6eee19d67b6faa1a
Sarita Sharma <sarita.sharma@xplortechnologies.com>
2026-01-23 11:23:55 -0500

    updated to use User.where(id: user.id).update_all(last_login_on: Time.now) instead of user.update_column

--- app/models/user.rb
+++ app/models/user.rb
@@ -179,9 +179,9 @@ class User < Principal
       end
     end
     if user && !user.new_record?
-        Rails.logger.info("DEBUG: user.class=#{user.class}, user.id=#{user.try(:id)}")
-        Rails.logger.info("DEBUG: Calling update_column(:last_login_on, Time.now) for user id=#{user.id}")
-      user.update_column(:last_login_on, Time.now)
+      Rails.logger.info("DEBUG: user.class=#{user.class}, user.id=#{user.try(:id)}")
+      Rails.logger.info("DEBUG: Calling update_all(last_login_on: Time.now) for user id=#{user.id}")
+      User.where(id: user.id).update_all(last_login_on: Time.now)
     end
     user
   rescue => text
@@ -192,9 +192,9 @@ class User < Principal
   def self.try_to_autologin(key)
     user = Token.find_active_user('autologin', key, Setting.autologin.to_i)
     if user
-        Rails.logger.info("DEBUG: user.class=#{user.class}, user.id=#{user.try(:id)}")
-        Rails.logger.info("DEBUG: Calling update_column(:last_login_on, Time.now) for user id=#{user.id}")
-      user.update_column(:last_login_on, Time.now)
+      Rails.logger.info("DEBUG: user.class=#{user.class}, user.id=#{user.try(:id)}")
+      Rails.logger.info("DEBUG: Calling update_all(last_login_on: Time.now) for user id=#{user.id}")
+      User.where(id: user.id).update_all(last_login_on: Time.now)
       user
     end
   end
