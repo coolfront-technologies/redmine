2a6b86a7dbdcbdcbc174d78e69950b0dfdc9a2df
Sarita Sharma <sarita.sharma@xplortechnologies.com>
2026-01-26 16:45:56 -0500

    Rails default to the current domain, set domain: nil:

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,3 @@
 # Explicitly set the session store for Redmine
-RedmineApp::Application.config.session_store :cookie_store, key: '_redmine_session', domain: :all, secure: true, same_site: :none
+#RedmineApp::Application.config.session_store :cookie_store, key: '_redmine_session', domain: :all, secure: true, same_site: :none
+Rails.application.config.session_store :cookie_store, key: '_redmine_session', domain: nil, secure: true
