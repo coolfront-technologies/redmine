beb1e71d43ce80130686faf1e2a84e174fc70bdb
Sarita Sharma <sarita.sharma@xplortechnologies.com>
2026-01-26 17:00:52 -0500

    patch for SSL detection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,12 @@
 # Settings specified here will take precedence over those in config/application.rb
 RedmineApp::Application.configure do
 
-  
+  # Force Rails to recognize HTTPS requests behind Azure proxy
+class Rack::Request
+  def ssl?
+    @env['HTTP_X_FORWARDED_PROTO'] == 'https' || @env['HTTPS'] == 'on'
+  end
+end
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
