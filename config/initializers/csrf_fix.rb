7b8578f1e7bfe655f38f4d424a6d3dc5d447763f
Jagan M Jadhav <jagan.jadhav@xplortechnologies.com>
2026-03-03 11:14:16 +0530

    FInal fix

--- config/initializers/csrf_fix.rb
+++ config/initializers/csrf_fix.rb
@@ -1,7 +1,8 @@
-# Disable CSRF origin check for Azure proxy compatibility
-# This is safe because Azure App Service is the only entry point
-begin
-  Rails.application.config.action_controller.forgery_protection_origin_check = false
-rescue NoMethodError
-  # Rails 3.2 doesn't have this option, skip it
+# Completely disable CSRF protection for login
+# Safe because Azure App Service is the only entry point
+
+Rails.application.config.after_initialize do
+  AccountController.class_eval do
+    skip_before_filter :verify_authenticity_token, :only => [:login]
+  end
 end
